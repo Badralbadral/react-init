5365b5648de30b294223432aba2d223622756769
Badral Tumursukh <23LP6834@G401-CC0100.local>
2024-01-08 11:54:29 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div>
+      <p>Hello World</p>
+      <Card />
+    </div>
+  );
+}
+
+import Card from "./components/Card";
---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+export default function Card() {
+  return (
+    <div>
+      <h1>Card</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. A nobis optio
+        impedit deserunt alias, dolores dignissimos iste sequi nam aperiam,
+        provident id distinctio! Corporis delectus nihil ipsum dicta laborum
+        sapiente.
+      </p>
+    </div>
+  );
+}
